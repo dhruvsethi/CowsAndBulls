09675263bce3bfe172028fe2f46ee3ec9ede7bb7
Dhruv Sethi <dsethi@paypalcorp.com>
2016-08-06 09:29:00 +0530

    Added main class and readme

---
+++ MainClass.java
@@ -0,0 +1,47 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
+public class MainClass {
+	final static String path = "C:\\Users\\dsethi\\Downloads\\sowpods.txt";
+
+	public static void main(String[] args) {
+		Map<String, Integer> difficultyWordLength = new HashMap<>();
+		difficultyWordLength.put("easy", 4);
+		difficultyWordLength.put("medium", 5);
+		difficultyWordLength.put("hard", 6);
+
+		System.out.println("Select difficulty - easy, medium or hard");
+		Scanner in = new Scanner(System.in);
+		String mode = in.next();
+		System.out.println("Mode selected - " + mode);
+		playCowsAndBulls(difficultyWordLength.get(mode));
+	}
+
+	private static void playCowsAndBulls(Integer wordLength) {
+		try {
+			System.out.println(getWordsOfLength(wordLength));
+		} catch (FileNotFoundException e) {
+			System.out.println("File Not Found");
+		}
+	}
+
+	private static List<String> getWordsOfLength(Integer wordLength) throws FileNotFoundException {
+		Scanner in = new Scanner(new File(path));
+		List<String> wordList = new ArrayList<>();
+		String word;
+		while (in.hasNext()) {
+			word = in.next();
+			if (word.length() == wordLength) {
+				wordList.add(word);
+			}
+		}
+		in.close();
+		return wordList;
+	}
+
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+#CowsAndBulls
+The game lets user choose difficulty - easy, medium or hard
+Based on difficulty word list is filtered on length
