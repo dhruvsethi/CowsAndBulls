c8a8515589af8a5ccc22f772c62340e10545a1af
Dhruv Sethi <dsethi@paypalcorp.com>
2016-08-06 10:02:04 +0530

    Updated getValidWords to return words with only unique letters

--- MainClass.java
+++ MainClass.java
@@ -1,10 +1,14 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.Set;
 
 public class MainClass {
 	final static String path = "C:\\Users\\dsethi\\Downloads\\sowpods.txt";
@@ -24,19 +28,22 @@ public class MainClass {
 
 	private static void playCowsAndBulls(Integer wordLength) {
 		try {
-			System.out.println(getWordsOfLength(wordLength));
+			System.out.println(getValidWords(wordLength));
 		} catch (FileNotFoundException e) {
 			System.out.println("File Not Found");
 		}
 	}
 
-	private static List<String> getWordsOfLength(Integer wordLength) throws FileNotFoundException {
+	private static List<String> getValidWords(Integer wordLength) throws FileNotFoundException {
 		Scanner in = new Scanner(new File(path));
 		List<String> wordList = new ArrayList<>();
+
+
 		String word;
 		while (in.hasNext()) {
 			word = in.next();
-			if (word.length() == wordLength) {
+			Set<String> uniqueLetters = new HashSet<>(Arrays.asList(word.split("")));     
+			if (word.length() == wordLength && uniqueLetters.size() == word.length()) {
 				wordList.add(word);
 			}
 		}
